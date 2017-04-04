9c14a513047bdfbdd74670bb98d7c53e942a9ff4
Alexander Boris <index.wow202@gmail.com>
2017-04-04 12:02:30 +0300

    Initialization of project and its setup

---
+++ src/js/internal.js
@@ -0,0 +1,15 @@
+(function($) {
+
+    var grid = {
+        xs: 480,
+        sm: 768,
+        md: 992,
+        lg: 1200
+    };
+    var elements = {
+
+    };
+    var options = {
+    };
+
+})(jQuery);
