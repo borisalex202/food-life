1bfd44fa259b88370613ceb1858409bee8fac715
Alexander Boris <index.wow202@gmail.com>
2017-04-10 09:14:19 +0300

    Layout and adaptive ‘Agreement’, ‘News’ and ‘Reports’ pages

--- src/js/external.js
+++ src/js/external.js
@@ -1,2 +1,3 @@
 @@include('./partials/_bootstrap.js');
+@@include('./partials/_sticky.js');
 @@include('../../bower_components/jquery.form-styler/jquery.formstyler.js');
--- src/js/internal.js
+++ src/js/internal.js
@@ -10,14 +10,56 @@
         siteHeader: $('.site-header'),
         iconMenu: $('.icon-menu'),
         mobileMenu: $('.mobile-menu'),
-        mobileParent: $('.mobile-menu').find('.has-child > a')
+        mobileParent: $('.mobile-menu').find('.has-child > a'),
+        agreement: $('.agreement'),
+        agreementBlock: $('.agreement-block'),
+        agreementMobile: $('.agreement-mobile'),
+        select: $('.select')
     };
     var options = {
-
+        headerHeight: elements.siteHeader.outerHeight(),
+        speedOffset: 700
     };
 
     @@include('./partials/_mobileMenu.js')
 
-    $('.select').styler();
+    $(window).on('load resize', function(){
+      if($(document).width() > grid.md) {
+        elements.agreement.stick_in_parent({
+            parent: $('.content-page-left'),
+            inner_scrolling: true,
+            offset_top: parseInt(options.headerHeight) + 10
+        });
+      } else {
+        elements.agreement.trigger("sticky_kit:detach")
+      }
+    });
+
+    $('.tab-nav a').click(function (e) {
+      e.preventDefault()
+      $(this).tab('show')
+    })
+
+    elements.select.styler();
+
+    elements.agreement.find('a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top - options.headerHeight - 10
+          }, options.speedOffset);
+          return false;
+        }
+      }
+    });
+
+    elements.agreementMobile.on('click', function(){
+      $(this).closest(elements.agreementBlock).toggleClass('active');
+    });
+    elements.agreement.find('a').on('click', function(){
+      $(this).closest(elements.agreementBlock).removeClass('active');
+    });
 
 })(jQuery);
